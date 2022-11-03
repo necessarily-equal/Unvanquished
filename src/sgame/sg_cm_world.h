866d03d952f8a1b4c22a7c816672028cb597e870
Antoine Fontaine <antoine.fontaine@epfl.ch>
2022-11-04 00:30:14 +0100

    Move documentation in src/sgame/sg_cm_world.h to make it more readable
    I haven't changed the content

--- src/sgame/sg_cm_world.h
+++ src/sgame/sg_cm_world.h
@@ -39,53 +39,49 @@ Maryland 20850 USA.
 
 #include "common/cm/cm_public.h"
 
-void G_CM_ClearWorld();
-
 // called after the world model has been loaded, before linking any entities
-
-void G_CM_UnlinkEntity( gentity_t *ent );
+void G_CM_ClearWorld();
 
 // call before removing an entity, and before trying to move one,
 // so it doesn't clip against itself
-
-void G_CM_LinkEntity( gentity_t *ent );
+void G_CM_UnlinkEntity( gentity_t *ent );
 
 // Needs to be called any time an entity changes origin, mins, maxs,
 // or solid.  Automatically unlinks if needed.
 // sets ent->r.absmin and ent->r.absmax
 // sets ent->leafnums[] for pvs determination even if the entity
 // is not solid
+void G_CM_LinkEntity( gentity_t *ent );
 
 clipHandle_t G_CM_ClipHandleForEntity( const sharedEntity_t *ent );
 
 void         G_CM_SectorList_f();
 
-int          G_CM_AreaEntities( const vec3_t mins, const vec3_t maxs, int *entityList, int maxcount );
-
 // fills in a table of entity numbers with entities that have bounding boxes
 // that intersect the given area.  It is possible for a non-axial bmodel
 // to be returned that doesn't actually intersect the area on an exact
 // test.
-// returns the number of pointers filled in
-// The world entity is never returned in this list.
-
-int G_CM_PointContents( const vec3_t p, int passEntityNum );
+//
+// The world entity is never included in this list.
+//
+// returns: the number of entities in the list
+int          G_CM_AreaEntities( const vec3_t mins, const vec3_t maxs, int *entityList, int maxcount );
 
 // returns the CONTENTS_* value from the world and all entities at the given point.
+int G_CM_PointContents( const vec3_t p, int passEntityNum );
 
+// mins and maxs are relative
+//
+// if the entire move stays in a solid volume, trace.allsolid will be set,
+// trace.startsolid will be set, and trace.fraction will be 0
+//
+// if the starting point is in a solid, it will be allowed to move out
+// to an open area
+//
+// passEntityNum, if isn't ENTITYNUM_NONE, will be explicitly excluded from clipping checks
 void G_CM_Trace( trace_t *results, const vec3_t start, const vec3_t mins, const vec3_t maxs,
                  const vec3_t end, int passEntityNum, int contentmask, int skipmask,
                  traceType_t type );
-
-// mins and maxs are relative
-
-// if the entire move stays in a solid volume, trace.allsolid will be set,
-// trace.startsolid will be set, and trace.fraction will be 0
-
-// if the starting point is in a solid, it will be allowed to move out
-// to an open area
-
-// passEntityNum, if isn't ENTITYNUM_NONE, will be explicitly excluded from clipping checks
 
 void G_CM_ClipToEntity( trace_t *trace, const vec3_t start, const vec3_t mins, const vec3_t maxs, const vec3_t end, int entityNum, int contentmask, traceType_t type );
 
